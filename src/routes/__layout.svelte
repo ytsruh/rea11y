250305d9db5b5d7493b75588bc91f943e6e5ddaa
Chris Hurst <ytsruh@gmail.com>
2022-02-26 13:54:57 +0000

    Created Navbar, Footer & Homepage

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,13 @@
 <script>
   import "../index.css";
   import Nav from "$lib/Nav.svelte";
+  import Footer from "$lib/Footer.svelte";
 </script>
 
 <div>
   <Nav />
-  <div class="bg-slate-50 text-coal dark:bg-neutral-900 dark:text-white min-h-screen">
+  <div class="bg-white text-coal dark:bg-neutral-900 dark:text-white min-h-screen">
     <slot />
   </div>
+  <Footer />
 </div>
