7a98c0308f33bc17b39ab32aa5005e5046088768
Chris Hurst <ytsruh@gmail.com>
2022-04-19 16:43:10 +0100

    Added Run Test Page

--- client/src/routes/app/run-test.svelte
+++ client/src/routes/app/run-test.svelte
@@ -2,10 +2,14 @@
   import Button from "$lib/components/Button.svelte";
   import { alert } from "$lib/stores/alert";
   import Loading from "$lib/Loading.svelte";
+  import ReportsTab from "$lib/reports/ReportsTab.svelte";
   const apiURL = import.meta.env.VITE_API_URL;
   const apiKey = "mUxo4Cy";
   let loading = false;
   let results = null;
+  let notices = [];
+  let warnings = [];
+  let errors = [];
   let testUrl = "";
   async function runTest() {
     const url = `${apiURL}/api/fullTest?key=${apiKey}`;
@@ -21,6 +25,18 @@
       const response = await res.json();
       loading = false;
       results = response.data;
+      for (let i = 0; i < results.issues.length; i++) {
+        const element = results.issues[i];
+        if (element.type === "notice") {
+          notices.push(element);
+        }
+        if (element.type === "warning") {
+          warnings.push(element);
+        }
+        if (element.type === "error") {
+          errors.push(element);
+        }
+      }
     } catch (err) {
       alert.set({ variant: "bg-danger", message: "Something went wrong, please try again" });
       loading = false;
@@ -50,9 +66,25 @@
 {/if}
 
 {#if results}
-  <div
+  <!-- <div
     class="w-full bg-white dark:bg-coal text-coal dark:text-white flex justify-center items-center my-5 rounded-md"
+  > -->
+  <div
+    class="grid grid-cols-1 md:grid-cols-2 gap-4 w-100 my-5 w-full bg-white dark:bg-coal text-coal dark:text-white rounded-md"
   >
-    Got results
+    <div class="text-center flex flex-col place-content-center">
+      <h2 class="text-xl py-2 mt-5 md:mt-0">Page Title : {results.documentTitle}</h2>
+    </div>
+    <div class="w-full flex flex-col items-center text-center px-5">
+      <div class="bg-secondary text-black rounded-md p-2 m-2 w-full">
+        Total Issues : {notices.length + warnings.length + errors.length}
+      </div>
+      <div class="bg-primary text-white rounded-md p-2 m-2 w-full">Notices : {notices.length}</div>
+      <div class="bg-warning text-white rounded-md p-2 m-2 w-full">Warnings : {warnings.length}</div>
+      <div class="bg-danger text-white rounded-md p-2 m-2 w-full">Errors : {errors.length}</div>
+    </div>
+  </div>
+  <div class="w-100 my-5">
+    <ReportsTab data={results.issues} />
   </div>
 {/if}
