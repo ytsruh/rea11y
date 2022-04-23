80da5cc6d295f0ced7d66c4cef6270aead871556
Chris Hurst <ytsruh@gmail.com>
2022-04-23 20:48:20 +0100

    Fixed bug on Report page

--- client/src/lib/reports/ReportItem.svelte
+++ client/src/lib/reports/ReportItem.svelte
@@ -4,7 +4,7 @@
   import { functions } from "$lib/helpers";
 </script>
 
-<div class="w-full p-2 flex flex-col">
+<div class="p-2 flex flex-col break-all">
   <hr class="border-1 w-3/4 place-self-center pb-2" />
   <h3><span class="text-lg">Item : </span>{index}</h3>
   <h3><span class="text-lg">HTML Location : </span>{data.context}</h3>
